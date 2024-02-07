82def0be37ff506adb3806e3397f5e2e3f8fb4a7
Diego Veiga <diego.veiga@xmobots.com.br>
2024-02-06 21:53:56 -0300

    class 2: creating the cards and your content

--- src/app.tsx
+++ src/app.tsx
@@ -19,11 +19,12 @@ export function App() {
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: 'adpsojdalsdjaklsdklas',
+          }}
+        />
       </div>
     </div>
   )
