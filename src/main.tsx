df3bf0720cda3325188a808c24e916f531ca3e27
Diego Veiga <diego.veiga@xmobots.com.br>
2024-02-15 18:55:06 -0300

    add clerk in application

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import { Toaster } from 'sonner'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 import './index.css'
+import { dark } from '@clerk/themes'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <Toaster richColors />
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      appearance={{ baseTheme: dark }}
+    >
+      <App />
+      <Toaster richColors />
+    </ClerkProvider>
   </React.StrictMode>,
 )
